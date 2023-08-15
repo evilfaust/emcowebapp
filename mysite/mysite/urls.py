83fae8555586785fb1b3bb82ac23ffec78a99570
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-08-15 13:21:43 +1100

    Clean it!
    - Прошёлся Prettier'ом по коду
    - Удалил лишние изображения
    - Удалил лишние html файлы

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,12 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls.static import static
 from django.conf import settings
-from maps import views 
+from django.conf.urls.static import static
+from maps import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('maps/', include('maps.urls')),
-    path('', views.index_home, name='index_home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("maps/", include("maps.urls")),
+        path("", views.index_home, name="index_home"),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
