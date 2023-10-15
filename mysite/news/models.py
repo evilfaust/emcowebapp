99ccaa0064a99c36788b973fc5f2e338afb30f4b
jurikarti <jurikarti@gmail.com>
2023-10-15 15:40:04 +0300

    Патч/фикс/обновление
    Обновил навбар.
    Вместо трех кнопок я оставил одну. Кстати, теперь он везде одинаковый.(возможно стоит вернуть кнопку домой!)
    
    Оптимизировал некоторые моменты на мобильной версии.
    Ну по типу того, что кнопка изменяется в зависимости от устройства.
    
    Изменение Детальных новостей.
    Добавил блок на фон, как в вк/тг, так же поменял местами картинку и текст. Картинка кстати стала меньше. Еще добавлю, что сам блок тоже стал меньше, да бы выглядело это гармонично.

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class News(models.Model):
-    name = models.CharField(max_length=4000)
+    name = models.TextField(max_length=4000)
     images = models.ImageField(upload_to='images/', blank=True)
     images2 = models.ImageField(upload_to='images/', blank=True)
     images3 = models.ImageField(upload_to='images/', blank=True)
     images4 = models.ImageField(upload_to='images/', blank=True)
     images5 = models.ImageField(upload_to='images/', blank=True)
     images6 = models.ImageField(upload_to='images/', blank=True)
-    discription = models.CharField(blank=True, max_length=100000000)
+    discription = models.TextField(blank=True, max_length=100000000)
 
     def __str__(self):
         return f'{self.name} {self.discription}'
