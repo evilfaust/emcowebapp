d1f5c7db76322056b419aa865db0ca7a2b965cec
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-10-25 15:10:23 +1100

    Mini navbar update

--- client/src/widgets/Navbar/index.tsx
+++ client/src/widgets/Navbar/index.tsx
@@ -3,12 +3,15 @@ import { Item } from "./features/item/ui";
 
 import logo from "shared/images/header-logo.png";
 import "./index.scss";
+import { NavLink } from "react-router-dom";
 
 export const Navbar: React.FC = () => {
   return (
     <header>
       <nav className="navbar">
-        <img src={logo} alt="logo" />
+        <NavLink to={"/"}>
+          <img src={logo} alt="logo" />
+        </NavLink>
         <ul>
           {pages.map((page) => (
             <Item label={page.label} link={page.to} value={page.value} />
