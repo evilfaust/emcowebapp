7c2e6222a20f70cd33a62fda94d5943077be81fc
norikjorik <mronanand@gmail.com>
2023-11-01 01:21:59 +1100

    footer The and
    vs`

--- client/src/widgets/Footer/index.tsx
+++ client/src/widgets/Footer/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./index.scss";
+import emco from "./img/emco.png"
 
 export const Footer: React.FC = () => {
   return (
     <div className="footer-bottom">
       <div className="container">
-        
         <div className="text">
-          Этот проект разработан при поддержке сотрудников 
-            и учеников детского технопарка EMCO TECH
-          
+          Этот проект разработан при поддержке сотрудников
+          <div>и учеников детского технопарка EMCO TECH</div>
+          <div className="emco">
+          <img src={emco} alt="EMCO TECH"/>
+          </div>
         </div>
-        
       </div>
     </div>
   );
