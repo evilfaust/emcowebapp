dd11c01d7cefd00d8d51811decbaddcf992097ae
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-11-09 21:02:16 +0300

    Junk Counter - start

--- client/src/pages/Main/components/counter/ui.tsx
+++ client/src/pages/Main/components/counter/ui.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import dump from "shared/images/counter base.png"
-import head_dump from "shared/images/counter head.png"
+import dump from "shared/images/counter base.png";
+import head_dump from "shared/images/counter head.png";
 import "./ui.scss";
 
 export const Counter: React.FC = () => {
   return (
     <section>
       <div className="counter_head">
-        <img src={head_dump}/>
+        <img src={head_dump} alt="head" />
         <div className="counter_base">
-            <img src={dump} alt="xz"/>
-            <p className="text">Свалкам бой</p>
+          <img src={dump} alt="body" />
+          <p className="text">Свалкам бой</p>
         </div>
       </div>
     </section>
   );
 };
+
+// Counter - общий
+// Counter Head - изображение + текст
+// Counter Body - изображение + текст
+// Стили - текст поверх изображения
+// Стили - мобильная версия, все ли хорошо
