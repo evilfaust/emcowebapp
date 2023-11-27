ff5c43d694bc98bb69221ca63846a5b8337b8a42
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-11-27 17:28:02 +1100

    Counter update

--- client/src/pages/Map/components/counters/ui.tsx
+++ client/src/pages/Map/components/counters/ui.tsx
@@ -1,8 +1,6 @@
 import { size } from "@floating-ui/core";
 import { info } from "console";
 import React from "react";
-import dump from "shared/images/counter base.png";
-import head_dump from "shared/images/counter head.png";
 import wrapper_text from "shared/images/info-map.png";
 import wrapper from "shared/images/img_svalok.png";
 import "./ui.scss";
@@ -14,12 +12,8 @@ export const CounterNumber: React.FC = () => {
         <div className="wrapper">
           <img src={wrapper} />
           <div className="wrapper_counter">
-            <img
-              className="counter_head"
-              src={head_dump}
-              alt="Счетчик убранных свалок"
-            />
-            <img className="counter_base" src={dump} alt="Подставка" />
+            <h1>СВАЛОК УБРАЛИ:<span><strong>40</strong></span></h1>
+            
           </div>
         </div>
         <div className="wrapper">
