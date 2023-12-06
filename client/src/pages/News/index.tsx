d8baeede6f25ce61226f8535c08734f23e6c8c08
BobbyCodeEnjoer <Wrong.pepe@mail.ru>
2023-12-06 16:37:14 +1100

    Макет новостной ленты
    Мобильный режим- не работает
    текст - ломается и ломает разметку вниз и вправо (растягивает карточки)
    3 в Ряд - Да
    Все Одинаковые - Да

--- client/src/pages/News/index.tsx
+++ client/src/pages/News/index.tsx
@@ -1,5 +1,23 @@
+import { NewsSmall } from "shared/UI/NewsSmall";
+import "./index.scss";
+
 function News() {
-  return <></>;
+  return (
+    <div className="NewsLine flex">
+      <ul>
+        <li>
+          <NewsSmall />
+          <NewsSmall />
+          <NewsSmall />
+        </li>
+        <li>
+          <NewsSmall />
+          <NewsSmall />
+          <NewsSmall />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default News;
---
+++ client/src/shared/UI/NewsSmall/index.tsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+import NewsImage from "shared/images/main-mobile.png";
+import { Card } from "react-bootstrap";
+
+export const NewsSmall = () => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={NewsImage} />
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        {''}
+      </Card.Text>
+    </Card>
+  );
+};
