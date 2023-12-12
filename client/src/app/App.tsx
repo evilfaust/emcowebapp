cc741ef9288dd278f4bbb86abef4bedad7d150a4
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-12-12 15:52:32 +1100

    Fixes for merge

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,9 +1,8 @@
+import { CssBaseline } from "@mui/material";
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
-
 import { LoadingSpinner } from "shared/UI";
-import { Footer, MobileNavigation, Navbar } from "widgets";
+import { Footer, MobileNavigation, Navigation } from "widgets";
 
 const Main = React.lazy(() => import("pages/Main/index"));
 const Map = React.lazy(() => import("pages/Map/index"));
@@ -12,27 +11,25 @@ const Newsletter = React.lazy(() => import("pages/Newsletter/index"));
 
 function App() {
   return (
-    <>
-      <Suspense
-        fallback={
-          <div className="centered">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        <Navbar />
-        <MobileNavigation />
-        <CssBaseline />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="news" element={<News />} />
-          <Route path="news/1" element={<Newsletter />} />
-          <Route path="map" element={<Map />} />
-          <Route path="*" element={<Main />} />
-        </Routes>
-        <Footer />
-      </Suspense>
-    </>
+    <Suspense
+      fallback={
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <Navigation />
+      <MobileNavigation />
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="news" element={<News />} />
+        <Route path="news/1" element={<Newsletter />} />
+        <Route path="map" element={<Map />} />
+        <Route path="*" element={<Main />} />
+      </Routes>
+      <Footer />
+    </Suspense>
   );
 }
 
