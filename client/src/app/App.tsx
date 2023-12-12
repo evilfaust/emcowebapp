3331c05359519a307ca27a4663787d96a12911d4
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-12-12 15:47:55 +1100

    Try to fix conflict files

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,54 +1,18 @@
-import { CssBaseline } from "@mui/material";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
 import { LoadingSpinner } from "shared/UI";
 import { Footer, MobileNavigation, Navbar } from "widgets";
-import { Counter } from "pages/Main/components/counter/ui";
-import axios from 'axios';
-import useDjangoData from '../useDjangoData'; // Подстройте количество `../` в зависимости от расположения файла
 
 const Main = React.lazy(() => import("pages/Main/index"));
 const Map = React.lazy(() => import("pages/Map/index"));
 const News = React.lazy(() => import("pages/News/index"));
 const Newsletter = React.lazy(() => import("pages/Newsletter/index"));
 
-// Тут мы обьявляем джсону, что такие ваще есть
-interface YouTubeVideo {
-  title: string;
-  channel: string;
-}
 function App() {
-  const { data: details, loading } = useDjangoData('http://localhost:8000');
   return (
-
-
-
-    //ну вот собсна и сам код, который отображает выводы джанго
-    //проблема только одна - мне так везде писать??? Яж повешусь...
-    //Крч главное, что работает...
-    <div>
-      <header>Данные из Джанго</header>
-      <hr></hr>
-      {details.length > 0 ? (
-        details.map((output, id) => (
-          <div key={id}>
-            <div>
-              <h2>{output.title}</h2>
-              <p>{output.channel}</p>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
-
-
-
-
-
-
-
-
+    <>
       <Suspense
         fallback={
           <div className="centered">
@@ -59,18 +23,16 @@ function App() {
         <Navbar />
         <MobileNavigation />
         <CssBaseline />
-        <Counter />
-        <Footer />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/news" element={<News />}>
-            <Route path=":newsId" element={<Newsletter />} />
-          </Route>
-          <Route path="/map" element={<Map />} />
+          <Route path="news" element={<News />} />
+          <Route path="news/1" element={<Newsletter />} />
+          <Route path="map" element={<Map />} />
           <Route path="*" element={<Main />} />
         </Routes>
+        <Footer />
       </Suspense>
-    </div>
+    </>
   );
 }
 
