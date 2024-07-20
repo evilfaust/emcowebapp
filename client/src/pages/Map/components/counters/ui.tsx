27093cabf8a6f0386575092e0e3974c2e3adb763
norikjorik <113436790+norikjorik@users.noreply.github.com>
2024-07-20 07:41:57 +0300

    page map
    Убейте меня прошу , 70% ready

--- client/src/pages/Map/components/counters/ui.tsx
+++ client/src/pages/Map/components/counters/ui.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import wrapper_text from "shared/images/info-map.png";
 import wrapper from "shared/images/img_svalok.png";
+import potImage from "shared/images/counter base.png"; 
+import leafImage from "shared/images/counter head.png"; 
 import "./ui.scss";
 
 export const CounterNumber: React.FC = () => {
   return (
     <section>
       <div className="layout">
-        <div className="wrapper">
-          <img
-            src={wrapper}
-            className="centered"
-            style={{ marginLeft: "auto" }}
-          />
-
+        <div className="wrapper left">
+          <img src={wrapper} className="centered" alt="Свалки" />
+          <div className="overlapping-images">
+            <img src={leafImage} className="leaf-image" alt="Листок" />
+            <div className="number">40</div>
+            <img src={potImage} className="pot-image" alt="Горшок" />
+          </div>
         </div>
-        <div className="wrapper">
-          <img className="centered" src={wrapper_text} />
+        <div className="wrapper right">
+          <img className="centered" src={wrapper_text} alt="Текст" />
         </div>
       </div>
     </section>
